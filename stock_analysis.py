ebd69bd00986ab0518e3717a91738e6bec63acc6
MmSie <73790866+MmSie@users.noreply.github.com>
2020-11-11 17:27:01 +0100

    Update stock_analysis.py

--- stock_analysis.py
+++ stock_analysis.py
@@ -9,6 +9,7 @@ import tkinter.messagebox
 import webbrowser as wbb
 import random
 
+
 # Creating the graphical user interface for the program
 class StockAnalysis:
     # This method is called when an object is created from the class
